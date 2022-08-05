f5f467abe72a6284bb407c1776818c970bae6e78
Darok656 <cesar_ortize@hotmail.com>
2022-08-04 22:01:07 -0500

    otro arreglo

--- public/main.js
+++ public/main.js
@@ -2,6 +2,7 @@ var socket = io.connect("http://localhost:8080", { forceNew: true });
 
 socket.on("messages", function (data) {
   console.log(data);
+  render(data);
 });
 
 function render(data) {
